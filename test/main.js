5eaf79606338cba15ab58f60348e4a70aeadaa2f
googoid <gugoad@gmail.com>
2015-12-24 01:33:35 +0400

    Initial commit

---
+++ test/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const chai = require('chai')
+  , expect = chai.expect
+  , dateString = require('../');
+
+chai.use(dateString);
+
+describe('chai-date-string', () => {
+  describe('expect(a).to.be.a.validDateString()', () => {
+    it('should fail a is not valid date string', () => {
+      try {
+        expect('some random string').to.be.a.validDateString();
+      }
+      catch (err) {
+        return;
+      }
+
+      throw new Error('it should fail');
+    });
+
+    it('should pass if a is valid date string', () => {
+      expect('2015').to.be.a.validDateString();
+      expect('2015-12-12').to.be.a.validDateString();
+      expect('2015-12-12 00:00').to.be.a.validDateString();
+      expect('2015-12-12 00:00:12').to.be.a.validDateString();
+    });
+  });
+});
